9ef8bd22bcb938ea5e78dc44d2476d660e6e8580
Kareem <ksaifeldawlla@gmail.com>
2025-10-18 19:07:15 -0400

    help from  @qiwara
    Co-authored-by: Qi Wang Ye <qiwara@users.noreply.github.com>

---
+++ audio_processing/TranscriptionTest.py
@@ -0,0 +1,14 @@
+
+#Test Cases
+import Transcription
+import TranscriptionApp
+FileName = "Test.wav"
+TransFile = Transcription.TranScribe(FileName)
+print("Transcribed sentence:"+ TransFile)
+print("--------------")
+print("Word Count: "+str(TranscriptionApp.countWords(TransFile)))
+print("--------------")
+print("Count Average: "+str(TranscriptionApp.countAverage(TransFile)))
+print("--------------")
+print("Count Filler: "+str(TranscriptionApp.countFiller(TransFile)))
+print("--------------")
